23cd681b5c83e924491aefb45bc37e2cc70179be
zielvna <zielvna@gmail.com>
2024-04-26 12:44:56 +0200

    remove unused stories and create empty placeholder component

---
+++ src/components/EmptyPlaceholder/EmptyPlaceholder.stories.tsx
@@ -0,0 +1,17 @@
+import EmptyPlaceholder from './EmptyPlaceholder'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'EmptyPlaceholder',
+  component: EmptyPlaceholder
+} satisfies Meta<typeof EmptyPlaceholder>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    desc: 'Add your first position by pressing the button and start earning!'
+  }
+}
---
+++ src/components/EmptyPlaceholder/EmptyPlaceholder.tsx
@@ -0,0 +1,30 @@
+import { Grid, Typography } from '@mui/material'
+import empty from '@static/svg/empty.svg'
+import classNames from 'classnames'
+import React from 'react'
+import { useStyles } from './styles'
+
+export interface IProps {
+  desc: string
+  className?: string
+  style?: React.CSSProperties
+}
+
+export const EmptyPlaceholder: React.FC<IProps> = ({ desc, className, style }) => {
+  const { classes } = useStyles()
+
+  return (
+    <Grid
+      container
+      direction='column'
+      alignItems='center'
+      className={classNames(classes.wrapper, className)}
+      style={style}>
+      <img src={empty} className={classes.image} />
+      <Typography className={classes.title}>It's empty here...</Typography>
+      <Typography className={classes.desc}>{desc}</Typography>
+    </Grid>
+  )
+}
+
+export default EmptyPlaceholder
