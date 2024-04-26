2d9f7aab8fb86dbafc095cae66ed3b5325cee7f8
zielvna <zielvna@gmail.com>
2024-04-26 12:31:27 +0200

    add list page components

--- src/store/consts/utils.ts
+++ src/store/consts/utils.ts
@@ -1,1 +1,12 @@
+import { NetworkType } from './static'
+
 export const createLoaderKey = () => (new Date().getMilliseconds() + Math.random()).toString()
+
+export const getInvariantAddress = (network: NetworkType): string | null => {
+  switch (network) {
+    case NetworkType.TESTNET:
+      return '5FiTccBSAH9obLA4Q33hYrL3coPm2SE276rFPVttFPFnaxnC'
+    default:
+      return null
+  }
+}
