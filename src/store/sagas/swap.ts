c65db9576c4e0a6ad7a5a141ba5fb37af3a6d1e5
zielvna <zielvna@gmail.com>
2024-06-03 11:24:00 +0200

    fix swap scenario by correcting amount in calculations

--- src/store/sagas/swap.ts
+++ src/store/sagas/swap.ts
@@ -202,7 +202,7 @@ export function* handleSwapWithAZERO(): Generator {
 
     const sqrtPriceLimit = calculateSqrtPriceAfterSlippage(estimatedPriceAfterSwap, slippage, !xToY)
     let calculatedAmountIn = amountIn
-    if (byAmountIn) {
+    if (!byAmountIn) {
       calculatedAmountIn = calculateAmountInWithSlippage(amountIn, sqrtPriceLimit, !xToY)
     }
 
@@ -367,8 +367,6 @@ export function* handleGetSimulateResult(action: PayloadAction<Simulate>) {
           byAmountIn,
           xToY ? MIN_SQRT_PRICE : MAX_SQRT_PRICE
         )
-
-        console.log(result)
 
         if (result.globalInsufficientLiquidity) {
           errors.push(SwapError.InsufficientLiquidity)
