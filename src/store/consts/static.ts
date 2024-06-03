23dbcbd11db65e3391e399c4ca1792820b0b6ee8
zielvna <zielvna@gmail.com>
2024-06-03 17:45:18 +0200

    add safe transaction fee to azero balance

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -213,3 +213,5 @@ export const ALL_FEE_TIERS_DATA = FEE_TIERS.map((tier, index) => ({
 }))
 
 export const U128MAX = 2n ** 128n - 1n
+
+export const SAFE_TRANSACETION_FEE = BigInt(Math.ceil(0.005 * 10 ** 12))
