d312b8493886ffcd89c548657acf388fbd03ac3e
zielvna <zielvna@gmail.com>
2024-06-20 14:00:54 +0200

    fix eslint issues

--- .storybook/decorators.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-
-// export const withMaterialStyles = storyFn => (
-//   <StylesProvider injectFirst>{storyFn()}</StylesProvider>
-// )
-
-export const withStore = store => storyFn => <Provider store={store}>{storyFn()}</Provider>
