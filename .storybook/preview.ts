02c14c7b053b1f655b2e80367438c392e08cc5b0
zielvna <zielvna@gmail.com>
2024-06-20 15:45:24 +0200

    update storybook and fix react docgen issues

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import { withRouter } from 'storybook-addon-remix-react-router'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import { withThemeFromJSXProvider } from '@storybook/addon-themes'
 import { theme } from '../src/static/theme'
+import { withStore } from './decorators'
 
 const customViewports = {
   sx375: {
