a2fe3b3859950fb62d4b5780000e7806ca25d730
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-06-24 15:34:51 +0200

    remove cancel icon from loading snackbar

--- src/components/Snackbar/LoadingSnackbar/index.tsx
+++ src/components/Snackbar/LoadingSnackbar/index.tsx
@@ -1,10 +1,8 @@
-import { CustomContentProps, useSnackbar } from 'notistack'
-import React, { useCallback } from 'react'
-import icons from '@static/icons'
+import React from 'react'
+import { CustomContentProps } from 'notistack'
 
 import {
   StyledCircularProgress,
-  StyledCloseButton,
   StyledContainer,
   StyledSnackbarContent,
   StyledTitle
@@ -12,30 +10,19 @@ import {
 import { Grid } from '@mui/material'
 import { Box } from '@mui/system'
 
-const LoadingSnackbar = React.forwardRef<HTMLDivElement, CustomContentProps>(
-  ({ id, message }, ref) => {
-    const { closeSnackbar } = useSnackbar()
-
-    const handleDismiss = useCallback(() => {
-      closeSnackbar(id)
-    }, [id, closeSnackbar])
-
-    return (
-      <StyledSnackbarContent ref={ref} role='alert'>
-        <StyledContainer>
-          <Grid container alignItems='center'>
-            <Box ml={1}>
-              <StyledCircularProgress size={13} />
-            </Box>
-            <StyledTitle>{message}</StyledTitle>
-          </Grid>
-          <StyledCloseButton onClick={handleDismiss}>
-            <img src={icons.closeSmallIcon}></img>
-          </StyledCloseButton>
-        </StyledContainer>
-      </StyledSnackbarContent>
-    )
-  }
-)
+const LoadingSnackbar = React.forwardRef<HTMLDivElement, CustomContentProps>(({ message }, ref) => {
+  return (
+    <StyledSnackbarContent ref={ref} role='alert'>
+      <StyledContainer>
+        <Grid container alignItems='center'>
+          <Box ml={1}>
+            <StyledCircularProgress size={13} />
+          </Box>
+          <StyledTitle>{message}</StyledTitle>
+        </Grid>
+      </StyledContainer>
+    </StyledSnackbarContent>
+  )
+})
 
 export default LoadingSnackbar
