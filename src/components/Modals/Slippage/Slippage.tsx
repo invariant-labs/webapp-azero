8e62e89293bf58b2379d468f551829157ddc779a
zielvna <zielvna@gmail.com>
2024-07-05 09:18:31 +0200

    remove unnecessary imports from slippage component

--- src/components/Modals/Slippage/Slippage.tsx
+++ src/components/Modals/Slippage/Slippage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import useStyles from './style'
 import { Box, Button, Grid, Input, Popover, Typography } from '@mui/material'
 
