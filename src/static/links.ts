80611cdb90f8cd3cc2392ee5dcf5676ce3c45fbf
zielvna <zielvna@gmail.com>
2024-07-08 10:54:32 +0200

    replace twitter with x

--- src/static/links.ts
+++ src/static/links.ts
@@ -1,5 +1,5 @@
 export const social = {
-  twitter: 'https://twitter.com/invariant_labs',
+  x: 'https://x.com/invariant_labs',
   github: 'https://github.com/invariant-labs',
   discord: 'https://discord.gg/w6hTeWTJvG',
   telegram: 'https://t.me/+-C9-e6L08AY4YWI0',
