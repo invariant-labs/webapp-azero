d99e7097e15ce44dcecd03691dd4d85988e795a7
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-07-23 14:33:07 +0200

    add shadow for snackbars

--- src/components/Snackbar/CustomSnackbar/style.ts
+++ src/components/Snackbar/CustomSnackbar/style.ts
@@ -15,7 +15,8 @@ export const StyledSnackbarContent = styled(SnackbarContent)(({ theme }) => ({
   borderRadius: 15,
   ...typography.body2,
   marginBottom: 5,
-
+  boxShadow:
+    'rgba(0, 0, 0, 0.2) 0px 3px 5px -1px, rgba(0, 0, 0, 0.2) 0px 6px 10px 0px, rgba(0, 0, 0, 0.2) 0px 1px 18px 0px',
   '& .MuiCircularProgress-colorPrimary': {
     color: colors.invariant.textGrey
   },
