2283bc7cb782565ffd41eb1f83eeecf1a70dedfc
Jakub Wołodko <kuba8369@gmail.com>
2024-09-16 13:44:55 +0200

    chore: disabled Stats page and mainnet

--- src/pages/StatsPage/index.tsx
+++ src/pages/StatsPage/index.tsx
@@ -1,13 +1,16 @@
 import { Grid } from '@mui/material'
 import { useStyles } from './styles'
-import WrappedStats from '@containers/WrappedStats/WrappedStats'
+// import WrappedStats from '@containers/WrappedStats/WrappedStats'
+import comingSoon from '../../static/png/coming-soon.png'
 
 export const StatsPage: React.FC = () => {
   const { classes } = useStyles()
 
   return (
     <Grid container className={classes.container}>
-      <WrappedStats />
+      <img src={comingSoon} alt='Coming soon' />
+
+      {/* <WrappedStats /> */}
     </Grid>
   )
 }
