1eb780372703c0f2b6a9aa976f16356ac59b1124
Jakub Wołodko <kuba8369@gmail.com>
2024-09-17 16:40:49 +0200

    feat: add Vara logo and update chain constants

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -349,7 +349,8 @@ export const DEFAULT_NEW_POSITION_SLIPPAGE = '0.50'
 export const CHAINS = [
   { name: Chain.Solana, address: 'https://invariant.app/swap' },
   { name: Chain.AlephZero, address: 'https://azero.invariant.app/swap' },
-  { name: Chain.Eclipse, address: 'https://eclipse.invariant.app/swap' }
+  { name: Chain.Eclipse, address: 'https://eclipse.invariant.app/swap' },
+  { name: Chain.Vara, address: 'https://vara.invariant.app/swap' }
 ]
 
 export const enum SortTypePoolList {
