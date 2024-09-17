8ee4e6d87ea0707a9010b7717ef28be2fd53111d
Jakub Wołodko <kuba8369@gmail.com>
2024-09-17 13:06:32 +0200

    fix: update StatsPage to use WrappedStats component and remove coming soon image

--- src/pages/StatsPage/index.tsx
+++ src/pages/StatsPage/index.tsx
@@ -1,16 +1,16 @@
 import { Grid } from '@mui/material'
 import { useStyles } from './styles'
-// import WrappedStats from '@containers/WrappedStats/WrappedStats'
-import comingSoon from '../../static/png/coming-soon.png'
+import WrappedStats from '@containers/WrappedStats/WrappedStats'
+// import comingSoon from '../../static/png/coming-soon.png'
 
 export const StatsPage: React.FC = () => {
   const { classes } = useStyles()
 
   return (
     <Grid container className={classes.container}>
-      <img src={comingSoon} alt='Coming soon' />
+      {/* <img src={comingSoon} alt='Coming soon' /> */}
 
-      {/* <WrappedStats /> */}
+      <WrappedStats />
     </Grid>
   )
 }
