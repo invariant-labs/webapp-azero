ca63a715f7e054c220f826388e18ddc417769aa1
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-09-18 17:29:28 +0200

    fix wording

--- src/components/PositionsList/PositionsList.tsx
+++ src/components/PositionsList/PositionsList.tsx
@@ -108,7 +108,7 @@ export const PositionsList: React.FC<IProps> = ({
         <Grid className={classes.searchRoot}>
           <Grid className={classes.titleBar}>
             <Typography className={classes.title}>Your Positions</Typography>
-            <TooltipHover text='Total number of you liquidity positions'>
+            <TooltipHover text='Total number of your liquidity positions'>
               <Typography className={classes.positionsNumber}>{String(length)}</Typography>
             </TooltipHover>
           </Grid>
