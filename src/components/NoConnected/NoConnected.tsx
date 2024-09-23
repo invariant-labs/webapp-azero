5b91291626db3d22225f988d0acd738b14201196
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-09-23 16:32:54 +0200

    crate new placeholder for not connected wallet

--- src/components/NoConnected/NoConnected.tsx
+++ src/components/NoConnected/NoConnected.tsx
@@ -2,6 +2,7 @@ import { Button, Grid, Typography } from '@mui/material'
 import icons from '@static/icons'
 import classNames from 'classnames'
 import { useStyles } from './style'
+import { useNavigate } from 'react-router-dom'
 
 export interface INoConnected {
   onConnect: () => void
@@ -11,18 +12,34 @@ export interface INoConnected {
 export const NoConnected: React.FC<INoConnected> = ({ onConnect, descCustomText }) => {
   const { classes } = useStyles()
 
+  const navigate = useNavigate()
+
   return (
     <>
       <Grid className={classNames(classes.blur, 'blurLayer')} />
       <Grid className={classNames(classes.container, 'blurLayer')}>
         <Grid className={classNames(classes.root, 'blurInfo')}>
           <img className={classes.img} src={icons.NoConnected} alt='Not connected' />
-          <Typography className={classes.desc}>Wallet is not connected.</Typography>
+          <Typography className={classes.desc}>
+            Start exploring liquidity pools right now!
+          </Typography>
+
           {descCustomText?.length && (
-            <Typography className={classes.desc}>{descCustomText}</Typography>
+            <Typography className={classes.desc}>
+              Or, connect your wallet to see existing positions, and create a new one!
+            </Typography>
           )}
-          <Button className={classes.button} onClick={onConnect} variant='contained'>
-            Connect a wallet
+          <Button
+            className={classes.buttonPrimary}
+            onClick={() => {
+              navigate('/newPosition/0_01')
+            }}
+            variant='contained'>
+            Explore liquidity pools
+          </Button>
+
+          <Button className={classes.buttonSecondary} onClick={onConnect} variant='contained'>
+            Connect wallet
           </Button>
         </Grid>
       </Grid>
--- src/components/NoConnected/style.tsx
+++ src/components/NoConnected/style.tsx
@@ -38,10 +38,10 @@ export const useStyles = makeStyles()(() => ({
     lineHeight: '20px',
     color: colors.invariant.lightHover
   },
-  button: {
+  buttonPrimary: {
     height: 40,
     width: 200,
-    marginTop: 20,
+    marginTop: 16,
     color: colors.invariant.componentBcg,
     ...typography.body1,
     textTransform: 'none',
@@ -53,6 +53,27 @@ export const useStyles = makeStyles()(() => ({
       boxShadow: '0px 0px 16px rgba(239, 132, 245, 0.35)',
       '@media (hover: none)': {
         background: colors.invariant.pinkLinearGradientOpacity,
+        boxShadow: 'none'
+      }
+    }
+  },
+  buttonSecondary: {
+    width: 200,
+    height: 40,
+    marginTop: 12,
+    ...typography.body2,
+    color: colors.white.main,
+    textTransform: 'none',
+    backgroundColor: colors.invariant.light,
+    borderRadius: 14,
+
+    transition: '0.1s',
+    '&:hover': {
+      //
+      boxShadow: '0px 0px 16px rgba(255, 255, 255, 0.1)',
+      backgroundColor: colors.invariant.light,
+      filter: 'brightness(1.2)',
+      '@media (hover: none)': {
         boxShadow: 'none'
       }
     }
