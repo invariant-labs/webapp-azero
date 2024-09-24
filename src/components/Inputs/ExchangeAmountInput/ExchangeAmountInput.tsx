a4c6756e6f86d277889098fe3373380c69c6287d
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-09-24 08:55:55 +0200

    hide estimated balance price

--- src/components/Inputs/ExchangeAmountInput/ExchangeAmountInput.tsx
+++ src/components/Inputs/ExchangeAmountInput/ExchangeAmountInput.tsx
@@ -186,39 +186,41 @@ export const AmountInput: React.FC<IProps> = ({
             />
           )}
         </Grid>
-        <Grid className={classes.percentages} container alignItems='center' wrap='nowrap'>
-          {current ? (
-            priceLoading ? (
-              <img src={loadingAnimation} className={classes.loading} alt='loading' />
-            ) : tokenPrice ? (
-              <Tooltip
-                enterTouchDelay={0}
-                leaveTouchDelay={Number.MAX_SAFE_INTEGER}
-                title='Estimated USD Value of the Selected Tokens in Your Wallet'
-                placement='bottom'
-                classes={{
-                  tooltip: classes.tooltip
-                }}>
-                <Typography className={classes.caption2}>
-                  ~${formatNumber(usdBalance.toFixed(2))}
-                </Typography>
-              </Tooltip>
-            ) : (
-              <Tooltip
-                enterTouchDelay={0}
-                leaveTouchDelay={Number.MAX_SAFE_INTEGER}
-                title='Cannot fetch price of token'
-                placement='bottom'
-                classes={{
-                  tooltip: classes.tooltip
-                }}>
-                <Typography className={classes.noData}>
-                  <span className={classes.noDataIcon}>?</span>No data
-                </Typography>
-              </Tooltip>
-            )
-          ) : null}
-        </Grid>
+        {!hideBalance && (
+          <Grid className={classes.percentages} container alignItems='center' wrap='nowrap'>
+            {current ? (
+              priceLoading ? (
+                <img src={loadingAnimation} className={classes.loading} alt='loading' />
+              ) : tokenPrice ? (
+                <Tooltip
+                  enterTouchDelay={0}
+                  leaveTouchDelay={Number.MAX_SAFE_INTEGER}
+                  title='Estimated USD Value of the Selected Tokens in Your Wallet'
+                  placement='bottom'
+                  classes={{
+                    tooltip: classes.tooltip
+                  }}>
+                  <Typography className={classes.caption2}>
+                    ~${formatNumber(usdBalance.toFixed(2))}
+                  </Typography>
+                </Tooltip>
+              ) : (
+                <Tooltip
+                  enterTouchDelay={0}
+                  leaveTouchDelay={Number.MAX_SAFE_INTEGER}
+                  title='Cannot fetch price of token'
+                  placement='bottom'
+                  classes={{
+                    tooltip: classes.tooltip
+                  }}>
+                  <Typography className={classes.noData}>
+                    <span className={classes.noDataIcon}>?</span>No data
+                  </Typography>
+                </Tooltip>
+              )
+            ) : null}
+          </Grid>
+        )}
       </Grid>
     </>
   )
